e21fbd2c0063116ac3bc34765496f1cb4a244b55
SergioRyzhov <ryzhovs@tut.by>
2021-01-16 00:53:50 +0300

    add lesson-1 files

---
+++ lesson1/task_1.py
@@ -0,0 +1,9 @@
+user_string = input('Input string: ')
+user_number = int(input('Input number: '))
+string = 'qwerty'
+number = 5
+float_number = 5.17
+
+print(f'user string:{user_string},\nuser number:{user_number},'
+      f'\nstring:{string}, \nnumber:{number},'
+      f'\nfloat_number:{float_number}')
